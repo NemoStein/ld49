f17f673a3aaa963e42aba0c8918e58cd5d56ab29
NemoStein <contato@nemostein.com.br>
2021-10-03 01:20:29 -0300

    Counting time now

--- Assets/Scripts/GameControllet.cs
+++ Assets/Scripts/GameControllet.cs
@@ -7,9 +7,15 @@ public class GameControllet : MonoBehaviour
     public Interactable[] Interactables;
     public InputActions InputActions;
 
+    public float TimeScale;
+    public float DayStart = 8f;
+    public float DayEnd = 17f;
+    float CurrentTime;
+
     Interactable SelectedInteractable;
     VisualElement InteractionContainer;
     TextElement InteractableName;
+    TextElement TimerLabel;
 
     int currentInteractable = -1;
 
@@ -17,6 +23,7 @@ public class GameControllet : MonoBehaviour
     {
         UIDocument document = GetComponent<UIDocument>();
         InteractableName = document.rootVisualElement.Query<TextElement>("interactable-name").First();
+        TimerLabel = document.rootVisualElement.Query<TextElement>("timer").First();
         InteractionContainer = document.rootVisualElement.Query<VisualElement>("interaction-container").First();
         InteractionContainer.AddToClassList("hide");
 
@@ -24,6 +31,25 @@ public class GameControllet : MonoBehaviour
         InputActions.Gameplay.Select.performed += SelectAction;
         InputActions.Gameplay.Interact.performed += InteractAction;
         InputActions.Gameplay.Give.performed += GiveAction;
+    }
+
+    void Start()
+    {
+        CurrentTime = DayStart * 60;
+    }
+
+    void Update()
+    {
+        CurrentTime += TimeScale * Time.deltaTime;
+        if (CurrentTime > DayEnd * 60)
+        {
+            Debug.Log("It's over");
+        }
+
+        int hour = (int) CurrentTime / 60;
+        int minute = (int) CurrentTime % 60;
+
+        TimerLabel.text = $"<mspace=18px>{hour:D2}h{minute:D2}";
     }
 
     void SelectAction(InputAction.CallbackContext context)
